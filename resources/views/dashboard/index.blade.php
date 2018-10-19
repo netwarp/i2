41457fb2fc2527597090f25445057b0e2c54e17c
netwarp <resethread@gmail.com>
2018-10-19 21:17:14 +0200

    fix index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -15,7 +15,7 @@
 				<div class="card">
 					<div class="card-body">
 						<h1 class="text-center">Message{{ $messages > 1 ? 's' : '' }}: {{ $messages }}</h1>
-						<a href="{{ action('Dashboard\CardsController@index') }}" class="btn btn-primary btn-block">Voir les messages</a>
+						<a href="{{ action('Dashboard\MessagesController@index') }}" class="btn btn-primary btn-block">Voir les messages</a>
 					</div>
 				</div>
 			</div>
