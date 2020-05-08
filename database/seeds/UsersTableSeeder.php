83933d87846c2b8fe5d5a14ed884a3bad48d24bf
netwarp <resethread@gmail.com>
2020-05-08 15:44:46 +0200

    .

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,7 +16,7 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
         	'name' => 'admin',
         	'email' => 'admin@admin.com',
-        	'password' => '$2y$10$SLEKtArymI1gCjrTtDj78Ot2asL/Z/IHCS6dWhMN/4XLRXrw7Kthu'
+        	'password' => bcrypt('I_am_AweSome_Admin_1976')
         ]);
     }
 }
